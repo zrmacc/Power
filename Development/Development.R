dd5e05aacfef689f34fe707a5765677f56b90d68
Zachary McCaw <zmccaw@Zacharys-MBP.attlocal.net>
2022-01-05 22:16:07 -0500

    Logistic regression

--- Development/Development.R
+++ Development/Development.R
@@ -1,23 +1,19 @@
 # Purpose: Testing power calculations.
 library(Power)
 
-beta_x <- sqrt(0.05)
-cov_xz <- 0.5
-n <- 10
-max_n <- 1e3
-t1e <- 0.05
-var_resid <- 1
-var_x <- 1
-var_z <- 1
+bx <- sqrt(0.005)
+bz <- sqrt(0.050)
+rho <- 0.50
+pve <- bx^2 + bz^2 + 2 * bx * bz * rho
 
 PowerLinReg(
-  beta_x = beta_x,
-  cov_xz = cov_xz,
-  n = n,
+  beta_x = bx,
+  cov_xz = rho,
+  n = 1000,
   t1e = 0.05,
-  var_resid = var_resid,
-  var_x = var_x,
-  var_z = var_z
+  var_resid = 1 - pve,
+  var_x = 1,
+  var_z = 1
 )
 
 SampleSizeLinReg(
